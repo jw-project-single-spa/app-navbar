a0cef8bf5844a7380f6a5a35cfb25e0ee7b4cc34
Eduardo Dallmann <eduardodallmann@gmail.com>
2022-09-04 13:51:53 -0300

    new lib

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,6 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-    externals: [/@emotion/, /jotai/, /moment/, /axios/],
+    externals: ["@mui/material", "@emotion", "jotai", "moment"],
   });
 };
